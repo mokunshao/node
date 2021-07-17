350d09778e5b60d6e04d98c317ea016edc383799
mokunshao <mokunshao@foxmail.com>
2021-07-17 18:15:49 +0800

    backup

---
+++ src/app19.js
@@ -0,0 +1,4 @@
+const myModule2 = require('./myModule2');
+
+console.log(myModule2.myInfo);
+console.log(myModule2.myFunction(8));
---
+++ src/myModule2.js
@@ -0,0 +1,8 @@
+const myModule2 = {
+    myInfo: { name: 'jack', age: 20 },
+    myFunction: function (input) {
+        return input + 5;
+    },
+};
+
+module.exports = myModule2;
