3ae38f827210bc4deec9e1db08ac6b80fef2f28e
mokunshao <mokunshao@foxmail.com>
2021-09-05 18:02:16 +0800

    backup

---
+++ src/app5/app0.js
@@ -0,0 +1,36 @@
+const http = require('http');
+const io = require('socket.io');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    if (req.url === '/') {
+        fs.readFile('./client.html', 'utf-8', (err, data) => {
+            if (err) {
+                console.log(err);
+                return;
+            } else {
+                res.end(data.toString());
+            }
+        });
+    } else {
+        res.end('<html><body>Error</body></html>');
+    }
+});
+
+server.listen(3000, 'localhost');
+
+const ws = new io.Server(server);
+
+// ws.listen(server);
+
+ws.on('connection', (socket) => {
+    console.log('connection has been established');
+    socket.on('message', (message) => {
+        console.log('message: ', message);
+    });
+    socket.on('disconnect', () => {
+        console.log('connection has lost');
+    });
+    socket.send('hello client');
+});
